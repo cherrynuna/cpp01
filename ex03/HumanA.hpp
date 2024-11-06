766edfb9f15b45623e380b2da624e2a86aff03c4
cherrynuna <inyoungstar7@gmail.com>
2024-11-06 11:44:13 +0900

    03

--- ex03/HumanA.hpp
+++ ex03/HumanA.hpp
@@ -1,18 +1,18 @@
 #ifndef HUMANA_HPP
 # define HUMANA_HPP
 
-#include "Weapon.hpp"
+# include "Weapon.hpp"
 
 class HumanA
 {
-private:
-	std::string	name;
-	Weapon		&weapon;
-	
-public:
-	HumanA(std::string name, Weapon &weapon);
-	~HumanA();
-	void	attack(void) const;
+	private:
+		std::string	name;
+		Weapon		&weapon;
+		
+	public:
+		HumanA(std::string name, Weapon &weapon);
+		~HumanA();
+		void	attack(void) const;
 };
 
 #endif
